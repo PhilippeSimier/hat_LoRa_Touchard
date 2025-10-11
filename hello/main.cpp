ced191cc7c8563e2f3615d0335f0a80f654c487a
philippeSimier <philaure@wanadoo.fr>
2025-10-11 15:10:58 +0200

    improve main.cpp

--- hello/main.cpp
+++ hello/main.cpp
@@ -12,25 +12,35 @@
 
 using namespace std;
 
+void afficherTexte(SSD1306& oled, const string& ligne1, const string& ligne2) {
+    oled.begin();
+    oled.setTextSize(2);
+    oled << clear << ligne1 << "\n" << ligne2 << display;
+}
+
 int main(int argc, char** argv) {
+
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <ligne1> <ligne2>" << endl;
+        return EXIT_FAILURE;
+    }
 
     try {
 
         SSD1306 oled;
-
-        oled.begin();
-        oled.setTextSize(2);
-
-        oled << clear << argv[1] << "\n" << argv[2] << display;
-
+        afficherTexte(oled, argv[1], argv[2]);
+        sleep(1);
     }
-
-
     catch (const std::runtime_error &e) {
 
         cout << "Exception caught: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+    catch (...) {
+        cerr << "Erreur inconnue" << endl;
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
